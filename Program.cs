e42ed94b943f1031e0408d25af21f65895486245
nicolas <nickenway@gmail.com>
2017-07-15 23:58:25 +0300

    add droid

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Threading;
 
 namespace battleOfDroids
@@ -16,12 +17,38 @@ namespace battleOfDroids
 
     internal class BattleField
     {
+        private Droid droid = new Droid(1, 1);
+        
         public void show()
         {
             Console.WriteLine("000000000000");
             for(var i = 0;i<10;i++)
                 Console.WriteLine("0          0");
             Console.WriteLine("000000000000");
+            Console.SetCursorPosition(droid.getX(), droid.getY());
+            Console.Write('X');
+        }
+    }
+
+    internal class Droid
+    {
+        private int x;
+        private int y;
+
+        public Droid(int x, int y)
+        {
+            this.x = x;
+            this.y = x;
+        }
+
+        public int getX()
+        {
+            return x;
+        }
+        
+        public int getY()
+        {
+            return y;
         }
     }
 }
